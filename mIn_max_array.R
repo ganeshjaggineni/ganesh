1a256aedce439e0833f385dfec06ed4df8f24fbc
ganeshjaggineni <121171965+ganeshjaggineni@users.noreply.github.com>
2022-12-30 07:39:23 +0530

    Add files via upload
    Finding the minimum and maximum elements in the array.

---
+++ mIn_max_array.R
@@ -0,0 +1,6 @@
+#Finding the minimum and maximum elements in the array.
+data<-c(12,2424,42,34,34,24,23,53,434,634,645);  #creating data vector
+arr=array(data)   #typecast to array data
+print(min(arr))  #printing minimum element using min() function
+print(max(arr))  #printing maximum element using max() function
+          
