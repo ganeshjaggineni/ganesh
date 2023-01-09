695dc7d094a8f63e4ea386395c73011ce2a19709
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2023-01-09 21:30:59 +0530

    output functions like  print(),cat(),paste(),sprint()

---
+++ output_functions.R
@@ -0,0 +1,16 @@
+#output function
+#using print() function
+var<-"ganesh jaggineni"
+print(var)
+
+#using paste() function
+#print(paste("is a good boy",var))
+
+#using sprint()
+name="ganesh"
+sprintf("%s is studied in lbrce college",name)
+num=170
+sprintf("There are %d number of students studied in lbrce",num)
+
+#using cat() function
+cat("ganesh\ngani")
