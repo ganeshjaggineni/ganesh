81b0135396ef239e2a363f69319680ad56bdd306
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2023-02-10 10:59:39 +0530

    Add files via upload
    array of zeros using numpy

---
+++ array of zeros.py
@@ -0,0 +1,18 @@
+#importing libraries
+import numpy
+#creating array of zeros
+arr=numpy.zeros([10,4])
+#printing result
+print(arr)
+
+#output
+[[0. 0. 0. 0.]
+ [0. 0. 0. 0.]
+ [0. 0. 0. 0.]
+ [0. 0. 0. 0.]
+ [0. 0. 0. 0.]
+ [0. 0. 0. 0.]
+ [0. 0. 0. 0.]
+ [0. 0. 0. 0.]
+ [0. 0. 0. 0.]
+ [0. 0. 0. 0.]]
