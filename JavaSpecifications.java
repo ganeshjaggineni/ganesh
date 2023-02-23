de69a01bbb10f15026f202e2b6fa231cfb7b4a2e
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2023-02-23 23:20:45 +0530

    Write a Java program to check whether Java is installed on your computer.
    Write a Java program to check whether Java is installed on your computer.

---
+++ JavaSpecifications.java
@@ -0,0 +1,12 @@
+package javaprograms;
+
+public class JavaSpecifications {
+	public static void main(String a[])
+	{
+		System.out.println("\njava version: "+System.getProperty("java.version"));
+		System.out.println("java runtime version :"+System.getProperty("java.runtime.version"));
+		System.out.println("java Home:"+System.getProperty("java.home"));
+		System.out.println("java class path :"+System.getProperty("java.class.path"));
+	}
+
+}
